cec8cbae84b569b86c2ef53cfa803276bf416c1f
Marcin Ławniczak <marcin@lawniczak.me>
2017-02-05 16:17:47 +0100

    Strip comments

--- list.php
+++ list.php
@@ -44,7 +44,6 @@ $content = get_web_page($startUrl);
 
 $array = json_decode($content['content'], true);
 
-//var_dump($array);
 function convert_imgur_link($link) {
     //check if has extension
     $last5chars = substr($link, -5);
@@ -54,32 +53,24 @@ function convert_imgur_link($link) {
         return $link . '.png';
     }
     return $link;
-//    } else {
-//        //strip extension and force png
-//        return substr($link, 0, $strpos).'.png';
-//    }
 
 }
 function process ($comment) {
     $html = '<tr>';
-//    var_dump($comment);
-//    $html .= $comment['link_url'].'<br>';
     $link_url = convert_imgur_link($comment['link_url']);
-//    $html .= $link_url;
 
     $html .= '<div id="left_col"><td>Oryginalny obraz:'. $comment['link_title'] .'<br> <img src="'. $link_url .'" width="200px"></td></div>';
     $out = $comment['body'];
-//    var_dump($out);
+
     $startsAt = strpos($out, "[") + strlen("[");
     $endsAt = strpos($out, "]", $startsAt);
-//    var_dump($endsAt);
+
     $photoshopText = substr($out, $startsAt, $endsAt - $startsAt);
     $startsAt = strpos($out, "(") + strlen("(");
     $endsAt = strpos($out, ")", $startsAt);
     $photoshopLink = substr($out, $startsAt, $endsAt - $startsAt);
 
-    $html .= '<div id="right_col"><td>
-Zmieniony obraz:'.$photoshopText.'<br> <img src="'. $photoshopLink .'" width="200px"></td></div></div></tr>';
+    $html .= '<div id="right_col"><td>Zmieniony obraz:'.$photoshopText.'<br> <img src="'. $photoshopLink .'" width="200px"></td></div></div></tr>';
 
     return $html;
 }
