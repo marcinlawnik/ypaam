1a18efece597335252ae46a71156acfa71f10920
Marcin Ławniczak <marcin@lawniczak.me>
2017-02-05 16:30:28 +0100

    Code refactoring

--- list.php
+++ list.php
@@ -2,6 +2,11 @@
 
 $startUrl = 'https://www.reddit.com/user/Your_Post_As_A_Movie.json';
 
+// Array containing data extracted from posts
+$posts = [];
+
+// String of next identifier to process
+$nextPageId = '';
 //get json file
 function get_web_page( $url )
 {
@@ -56,10 +61,6 @@ function convert_imgur_link($link) {
 
 }
 function process ($comment) {
-    $html = '<tr>';
-    $link_url = convert_imgur_link($comment['link_url']);
-
-    $html .= '<div id="left_col"><td>Oryginalny obraz:'. $comment['link_title'] .'<br> <img src="'. $link_url .'" width="200px"></td></div>';
     $out = $comment['body'];
 
     $startsAt = strpos($out, "[") + strlen("[");
@@ -69,6 +70,21 @@ function process ($comment) {
     $startsAt = strpos($out, "(") + strlen("(");
     $endsAt = strpos($out, ")", $startsAt);
     $photoshopLink = substr($out, $startsAt, $endsAt - $startsAt);
+    $linkUrl = convert_imgur_link($comment['link_url']);
+
+    $posts[] = [
+        'link_author' => $comment['link_author'],
+        'link_url' => $comment['link_url'],
+        'link_url_converted' => $linkUrl,
+        'link_title' => $comment['link_title'],
+        'comment_body' => $comment['body'],
+        'photoshop_link' => $photoshopLink,
+        'photoshop_text' => $photoshopText
+    ];
+    $html = '<tr>';
+
+    $html .= '<div id="left_col"><td>Oryginalny obraz:'. $comment['link_title'] .'<br> <img src="'. $linkUrl .'" width="200px"></td></div>';
+
 
     $html .= '<div id="right_col"><td>Zmieniony obraz:'.$photoshopText.'<br> <img src="'. $photoshopLink .'" width="200px"></td></div></div></tr>';
 
@@ -91,6 +107,10 @@ echo '<html><head>
 </style>
 </head><body><table>
 ';
+
+// Save next page identifier
+
+$nextPageId = $array['data']['after'];
 foreach ($array['data']['children'] as $comment) {
      echo process($comment['data']);
 }
